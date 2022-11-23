68602e0e349790d5834c913f32e15ebcb8842a87
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-11-23 19:39:12 -0300

    Implement simple CRUD operations without any rules

--- src/main/java/com/fronchak/api/controllers/CategoryController.java
+++ src/main/java/com/fronchak/api/controllers/CategoryController.java
@@ -1,9 +1,20 @@
 package com.fronchak.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fronchak.domain.entities.Category;
 import com.fronchak.domain.services.CategoryService;
 
 @RestController
@@ -12,5 +23,32 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryService service;
+	
+	@PostMapping(value = "/v1")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Category save(@RequestBody Category category) {
+		return service.save(category);
+	}
+	
+	@GetMapping(value = "/v1/{id}")
+	public Category findById(@PathVariable(name = "id") Long id) {
+		return service.findById(id);
+	}
+	
+	@GetMapping(value = "/v1")
+	public List<Category> findAll() {
+		return service.findAll();
+	}
+	
+	@PutMapping(value = "/v1")
+	public Category update(@RequestBody Category category) {
+		return service.update(category);
+	}
+	
+	@DeleteMapping(value = "/v1/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(name = "id") Long id) {
+		service.deleteById(id);
+	}
 	
 }
--- src/main/java/com/fronchak/domain/services/CategoryService.java
+++ src/main/java/com/fronchak/domain/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.fronchak.domain.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fronchak.domain.entities.Category;
 import com.fronchak.domain.repositories.CategoryRepository;
 
 @Service
@@ -10,4 +13,24 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
+	
+	public Category save(Category category) {
+		return repository.save(category);
+	}
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		return repository.findById(id).get();
+	}
+	
+	public Category update(Category category) {
+		return repository.save(category);
+	}
+	
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
 }
